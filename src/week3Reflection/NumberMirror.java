c2103b3e0720170b119cf34267bba76fcac0057c
5Rabie5 <n.rabietarboush@gmail.com>
2018-11-23 10:13:06 +0100

    new upload

--- src/week3Reflection/NumberMirror.java
+++ src/week3Reflection/NumberMirror.java
@@ -1,4 +1,52 @@
 package week3Reflection;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class NumberMirror {
+    String result;
+    Integer number;
+
+    public Integer mirror(Integer number) {
+        String numberToString = number.toString();
+        String[] split = numberToString.split("");
+        List<String> list = Arrays.asList(split);
+        ArrayList<String> numberToStrings = new ArrayList<>(list);
+        Collections.reverse(numberToStrings);
+        numberToString = String.join("", numberToStrings);
+        Integer result = Integer.valueOf(numberToString);
+        return result;
+    }
+
+    public Integer specialMirror(Integer number) {
+        String numberToString = number.toString();
+        String[] split = numberToString.split("");
+        List<String> list = Arrays.asList(split);
+        ArrayList<String> numberToStrings = new ArrayList<>(list);
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+         for (numberToString:
+               numberToStrings) {
+             numbers.add(Integer.valueOf(numberToString));
+          }
+
+            for (Integer number3:  numbers) {
+        if (number3 < 9) {
+            number3 = number3 + 1;
+        }
+       number3 = 0;
+   }
+
+
+        Collections.reverse(numbers);
+    Integer number1 =Integer.reverseBytes(234432);
+        System.out.println(number1);
+    numberToString =String.join("",numberToStrings);
+    Integer result = Integer.valueOf(numberToString);
+        return result;
+}
+
+
 }
--- src/week3Reflection/NumberMirrorApplication.java
+++ src/week3Reflection/NumberMirrorApplication.java
@@ -1,4 +1,19 @@
 package week3Reflection;
 
+import java.util.Scanner;
+
 public class NumberMirrorApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("pleas enter Number");
+        Integer number = scanner.nextInt();
+        System.out.println("Your Number its : " + number);
+        NumberMirror numberMirror = new NumberMirror();
+
+        Integer mirroredNumber = numberMirror.mirror(number);
+        Integer mirroredNumber2 = numberMirror.specialMirror(number);
+
+        System.out.println("Your  mirrored Number its : " + mirroredNumber);
+        System.out.println("Your  mirrored pecial Number its : " + mirroredNumber2);
+    }
 }
